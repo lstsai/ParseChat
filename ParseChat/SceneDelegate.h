f948b512504d9d2b3ccb61e9269a5f4fc664a477
Lauren Tsai <laurentsai@fb.com>
2020-07-06 09:01:54 -0700

    Initial Commit

---
+++ ParseChat/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  ParseChat
+//
+//  Created by laurentsai on 7/6/20.
+//  Copyright © 2020 laurentsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ ParseChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParseChat
+//
+//  Created by laurentsai on 7/6/20.
+//  Copyright © 2020 laurentsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
